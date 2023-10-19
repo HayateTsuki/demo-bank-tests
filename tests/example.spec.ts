81d6dccb608075745d097fa89119aee679e16b01
Mateusz <mateusz.krzeszowiec@1450.pl>
2023-10-19 09:49:55 +0200

    Changed playwright.config.ts and refactor tests/example.scpec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test';
 
+
 test('test', async ({ page }) => {
   await page.goto('https://demo-bank.vercel.app/');
   await page.getByTestId('login-input').click();
-  await page.getByTestId('login-input').fill('adminadm');
+  await page.getByTestId('login-input').fill('testerLO');
   await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('adminadm');
+  await page.getByTestId('password-input').fill('10987654');
   await page.getByTestId('login-button').click();
+  await page.getByTestId('user-name').click();
+
 
   await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
 });
