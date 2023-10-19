f34669786aa2a2a63aaa047b898057eeda64c829
Mateusz <mateusz.krzeszowiec@1450.pl>
2023-10-19 14:52:14 +0200

    Refactor quick payment with correct data test appropriate to AAA pattern

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,34 +1,55 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Pulpit tests', () => {
-    test("quick payment with correct data", async ({ page }) => {
-        await page.goto("https://demo-bank.vercel.app/");
-        await page.getByTestId('login-input').fill('testerLO');
-        await page.getByTestId("password-input").fill("10987654");
-        await page.getByTestId("login-button").click();
-    
-        await page.getByRole('link', { name: 'szybki przelew' }).click();
-        await page.locator('#widget_1_transfer_receiver').selectOption('2');
-        await page.locator('#widget_1_transfer_amount').fill('150');
-        await page.locator('#widget_1_transfer_title').fill('Przelew środków');
-        await page.getByRole('button', { name: 'wykonaj' }).click();
-        await page.getByTestId('close-button').click();
-        await page.getByRole('link', { name: 'Przelew wykonany! Chuck Demobankowy - 150,00PLN - Przelew środków' }).click();
-      });
+test.describe("Pulpit tests", () => {
+  test("quick payment with correct data", async ({ page }) => {
+    //Arrange
+    const url = "https://demo-bank.vercel.app/";
+    const userId = "testerLO";
+    const userPassword = "10987654";
+    const receiverId = "2";
+    const transferAmount = "150";
+    const transferTitle = "Przelew środków";
+    const expectedTransferReceiver =
+      "Chuck Demobankowy";
 
-      test("successful mobile top-up", async ({ page }) => {
-        await page.goto("https://demo-bank.vercel.app/");
-        await page.getByTestId('login-input').fill('testerLO');
-        await page.getByTestId("password-input").fill("10987654");
-        await page.getByTestId("login-button").click();
+    //Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userId);
+    await page.getByTestId("password-input").fill(userPassword);
+    await page.getByTestId("login-button").click();
+
+    await page
+      .locator("#widget_1_transfer_receiver")
+      .selectOption(receiverId);
+    await page.locator("#widget_1_transfer_amount").fill(transferAmount);
+    await page.locator("#widget_1_transfer_title").fill(transferTitle);
     
-        await page.getByRole('link', { name: 'doładowanie telefonu' }).click();
-        await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
-        await page.locator('#widget_1_topup_amount').fill('50');
-        await page.locator('#uniform-widget_1_topup_agreement span').click();
-        await page.getByRole('button', { name: 'doładuj telefon' }).click();
-        await page.getByTestId('close-button').click();
-        
-        await expect(page.locator('#show_messages')).toHaveText("Doładowanie wykonane! 50,00PLN na numer 500 xxx xxx");
-      });
+    await page.getByRole("button", { name: "wykonaj" }).click();
+    await page.getByTestId("close-button").click();
+
+    //Assert
+    await expect(page.locator('#show_messages')).toHaveText(`Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`);
+  });
+
+  test("successful mobile top-up", async ({ page }) => {
+    const url = "https://demo-bank.vercel.app/";
+    const userId = "testerLO";
+    const userPassword = "10987654";
+
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userId);
+    await page.getByTestId("password-input").fill(userPassword);
+    await page.getByTestId("login-button").click();
+
+    await page.getByRole("link", { name: "doładowanie telefonu" }).click();
+    await page.locator("#widget_1_topup_receiver").selectOption("500 xxx xxx");
+    await page.locator("#widget_1_topup_amount").fill("50");
+    await page.locator("#uniform-widget_1_topup_agreement span").click();
+    await page.getByRole("button", { name: "doładuj telefon" }).click();
+    await page.getByTestId("close-button").click();
+
+    await expect(page.locator("#show_messages")).toHaveText(
+      "Doładowanie wykonane! 50,00PLN na numer 500 xxx xxx"
+    );
+  });
 });
